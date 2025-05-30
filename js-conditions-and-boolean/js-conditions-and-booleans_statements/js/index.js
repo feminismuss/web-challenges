609b69aa4e5baa4b845726c2596228bf26b7791c
Hannah Capell <hannah.capell@gmail.com>
2025-05-30 16:44:46 +0200

    part 3: Hotdogs, done

--- js-conditions-and-boolean/js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-boolean/js-conditions-and-booleans_statements/js/index.js
@@ -23,7 +23,28 @@ if (number / 2 == 0) {
 }
 
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
+// - for a given `numberOfHotdogs` a person buys, log the respective price that the person has to pay:
+// - for less than 5 hotdogs: 2 euro per hotdog
+// - for at least 5 but less than 100 hotdogs: 1.50 euro per hotdog
+// - for at least 100 but less than 1 000 000 hotdogs: 1 euro per hotdog
+// - for at least 1 000 000 hotdogs: 0.10 euro per hotdog
+// - change the `numberOfHotdogs` to see if your code works properly
+
+const numberOfHotdogs = 1000001;
+
+if (numberOfHotdogs <= 5) {
+  price = numberOfHotdogs * 2;
+  console.log(price);
+} else if (numberOfHotdogs <= 100) {
+  price = numberOfHotdogs * 1.5;
+  console.log("Deine Hotdogs kosten:", price);
+} else if (numberOfHotdogs <= 1000000) {
+  price = numberOfHotdogs * 1;
+  console.log("Deine Hotdogs kosten:", price);
+} else if (numberOfHotdogs >= 1000000) {
+  price = numberOfHotdogs * 0.1;
+  console.log("Deine Hotdogs kosten:", price);
+}
 
 // Part 4: Daytime
 const currentHour = 12;
