0e90a24811facaeb200ab8751df85bca0260c43f
Hannah Capell <hannah.capell@gmail.com>
2025-06-04 11:39:14 +0200

    react to submission

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,14 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+
+form.addEventListener("submit", (event) => {
+event.preventDefault();
+const formData = new FormData(event.target);
+const data = Object.fromEntries(formData);
+
+event.target.reset();
+event.target.elements.firstName.focus();
+console.log("Data: ", data);
+});
