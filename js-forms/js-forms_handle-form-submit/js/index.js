1580f9a0c97f6321eabd9a65e933e5b0fdf35d76
Hannah Capell <hannah.capell@gmail.com>
2025-06-04 12:34:19 +0200

    approach to bonus task

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,7 +1,11 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+// const badnessInput  = document.querySelector('[data-js="badness"]');
+// const ageInput = document.querySelector('[data-js="age"]');
 
+// ageBadnessSum = badnessInput + ageInput;
+// console.log(ageBadnessSum);
 
 form.addEventListener("submit", (event) => {
 event.preventDefault();
@@ -12,3 +16,9 @@ event.target.reset();
 event.target.elements.firstName.focus();
 console.log("Data: ", data);
 });
+
+// ## Bonus: Calculate and log the age-badness-sum 🤷‍♀️
+
+// The formula is: `age + badness`
+// Log to the console _(replace the values between the quotes)_:
+// > The age-badness-sum of "firstName" is "age-badness-sum"
