b337004bd6595eb502f88a35563e23bc6eb0c8ea
Hannah Capell <hannah.capell@gmail.com>
2025-06-17 12:49:23 +0200

    render an article

--- react-basics/react-basics_hello-world-article/src/App.jsx
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,11 @@
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle/>;
+}
+function HelloWorldArticle() {
+  return (<article>
+    <h1>Segeln als Familie</h1>
+    <p>Gemeinsames Segeln kann ein unvergessliches Abenteuer für die ganze Familie sein.</p>
+  </article>);
 }
