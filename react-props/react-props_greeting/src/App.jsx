d6d4a3ff163e38c52e2cc6c2043cb7d5333da7f9
Hannah Capell <hannah.capell@gmail.com>
2025-06-17 15:42:24 +0200

    conditional rendering with props

--- react-props/react-props_greeting/src/App.jsx
+++ react-props/react-props_greeting/src/App.jsx
@@ -1,7 +1,14 @@
 export default function App() {
-  return <Greeting name="Hannah"/>;
+  return (
+    <div>
+      <Greeting name="Hannah" />
+      <Greeting name="Roland" />
+    </div>
+  );
 }
+const coaches = ["Roland", "Sven", "Gimena"];
 
-function Greeting ({name}){
-  return <h1>Hello, {name}!</h1>
+function Greeting({ name }) {
+  const isCoach = coaches.includes(name);
+  return <h1>{isCoach ? "Hello, Coach!" : `Hello, ${name}!`}</h1>;
 }
