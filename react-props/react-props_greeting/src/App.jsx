c5c5e1dbeafeb4117881bc3d597ce10f2524b5d0
Hannah Capell <hannah.capell@gmail.com>
2025-06-17 15:28:54 +0200

    Greeting component with props

--- react-props/react-props_greeting/src/App.jsx
+++ react-props/react-props_greeting/src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Hannah"/>;
+}
+
+function Greeting ({name}){
+  return <h1>Hello, {name}!</h1>
 }
