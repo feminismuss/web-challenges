fa1e0931060f7f9eadac7ce9910acbd8224c04f3
Hannah Capell <hannah.capell@gmail.com>
2025-06-18 11:51:40 +0200

    add children as props to button component

---
+++ react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <main>
+      <Button>Butter</Button> 
+      <Button>Milch</Button>
+      <Button>Zucker</Button>
+      <Button>Eier</Button>
+    </main>
+  );
+}
+
+function Button({children}) {
+  return (
+    <button className="button" type="button">
+      Click me!
+    </button>
+  );
+}
